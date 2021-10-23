ec9719898adeb0f16d1ece246119fd08707ec202
Nadine <nadine_paez@hotmail.com>
2021-10-23 16:05:32 -0400

    obtain user input

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,15 +2,18 @@
 const Employee = require ('../lib/Employee');
 
 test('Can instantiate Employee instance', () => {
-    const employee = new Employee('Dave');
+    const employee = new Employee('Dave', 1234, 'a@a.com');
 
     //test for name, id, email
     expect(employee.name).toBe('Dave');
-    expect(employee.id).toBe(expect.any(Number));
+    expect( typeof employee.id).toBe('number');
     expect(employee.email).toBe(expect.any(String));
     
     // //test for methods:
     // test('Can set name via constructor arguments', () => {
+        //const emplyee = new Employee('Dave');
 
-    // })
+        //test for getName()
+        //expect(employee.getName()).toHaveProperty('name');
+// })
 })
