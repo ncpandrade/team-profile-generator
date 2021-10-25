e1b0bf983611af28c8025537292390d982fb29ce
Nadine <nadine_paez@hotmail.com>
2021-10-24 23:32:45 -0400

    create html file

--- src/page-template.js
+++ src/page-template.js
@@ -1,25 +1,41 @@
 //create helper function for each type of employee
 //Manager
 const managerTemplate = (employees) => `
-<div>
-  <h1>${employees.name}</h1>
-  <h2>${employees.role}<h2>
+<div class ="card-body">
+  <h1 class="card-title bg-primary">${employees.name}</h1>
+  <h2 class = "card-subtitle">${employees.getRole()}<h2>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${employees.id}</li>
+    <li class="list-group-item"><a href="${employees.email}">${employees.email}</a></li>
+    <li class="list-group-item">${employees.officeNumber}</li>
+  </ul>
 <div>
 `
 
 //Engineer
 const engineerTemplate = (employees) => `
-<div>
-  <h1>${employees.name}</h1>
-  <h2>${employees.role}<h2>
+<div class ="card-body">
+  <h1 class="card-title bg-primary">${employees.name}</h1>
+  <h2 class = "card-subtitle">${employees.getRole()}<h2>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${employees.id}</li>
+    <li class="list-group-item"><a href="${employees.email}">${employees.email}</a></li>
+    <li class="list-group-item"><a href="https://github.com/${employees.github}">Github</a></li>
+  </ul>
+  
 <div>
 `
 
 //Intern
 const internTemplate = (employees) => `
-<div>
-  <h1>${employees.name}</h1>
-  <h2>${employees.role}<h2>
+<div class ="card-body">
+  <h1 class="card-title bg-primary">${employees.name}</h1>
+  <h2 class = "card-subtitle">${employees.getRole()}<h2>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${employees.id}</li>
+    <li class="list-group-item"><a href="${employees.email}">${employees.email}</a></li>
+    <li class="list-group-item">${employees.school}</li>
+  </ul>
 <div>
 `
 
@@ -32,26 +48,32 @@ const generatePage = (employees) => `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Team-Profile</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 </head>
 
 <body>
-//getting error cannot read property of map
-//is this b/c this.employees is not correct, does not include data yet?
-  ${employees.map((empObj) => {
-  if (employees.role === 'Manager') {
-    managerTemplate(empObj)
-  }
-  else if (employees.role === 'Engineer') {
-    engineerTemplate(empObj)
-  }
-  else {
-    internTemplate(empObj)
-  }
-})
-  }
-
-
-  <h2><a href="https://github.com/">Github</a></h2>
+<div class="row">
+  <div class="col-sm-6">
+    <div class = card>
+      ${employees.map((empObj) => {
+      if (employees.role === 'Manager') {
+        return managerTemplate(empObj)
+      }
+       else if (employees.role === 'Engineer') {
+         return engineerTemplate(empObj)
+       }
+       else {
+         return internTemplate(empObj)
+       }
+      })
+      }
+      </div>
+    </div>
+  </div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
 `;
