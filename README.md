739bec3fa2bcccf78178475963e9ad515dd36b2a
Nadine <nadine_paez@hotmail.com>
2021-10-31 12:34:11 -0400

    README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Team Profile Generator
+
+## Video Demo
+https://watch.screencastify.com/v/ua6bxxl7ZSYFYkGs2YOt
+
+## Description
+Build a command-line application to manage a company's employee profile, using Node.js, Inquirer, and Jest.
+
+## Installation
+- Download the code
+- Run npm install
+- Run inquirer install
+- Run npm install --dev jest
+
+## Usage
+Use to view and manage the departments, roles, and employees in the company in order to organize and plan for the business. To use, run node index.js in terminal. You will be promted to enter information and an html page is generated in the dist folder. Open in browser to view result.
+
+## Test
+Jest was used for creating test suites for each class type. To run tests, install the Jest package, add "jest" to the test script in the package JSON and run npm run test in the command line.
--- src/page-template.js
+++ src/page-template.js
@@ -4,14 +4,14 @@ const managerTemplate = (employees) => `
 <div class="col-sm-4" style="margin-top: 50px;">
   <div class="card" style="width: 20rem;">
   <div class="card-header bg-primary">
-  <h1 class="card-title bg-primary">${employees.name}</h1>
-  <h2 class = "card-subtitle"><i class="fas fa-mug-hot"></i>${employees.getRole()}<h2>
+  <h1 class="card-title bg-primary text-white">${employees.name}</h1>
+  <h2 class = "card-subtitle text-white"><i class="fas fa-mug-hot"></i>${employees.getRole()}<h2>
   </div>
   <div class="card-body">
     <ul class="list-group text-left">
-      <li class="list-group-item">ID:${employees.id}</li>
-      <li class="list-group-item">Email:<a href="mailto:${employees.email}">${employees.email}</a></li>
-      <li class="list-group-item">Office number:${employees.officeNumber}</li>
+      <li class="list-group-item">ID: ${employees.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${employees.email}">${employees.email}</a></li>
+      <li class="list-group-item">Office number: ${employees.officeNumber}</li>
     </ul>
     </div>
   </div>
@@ -19,39 +19,39 @@ const managerTemplate = (employees) => `
   `
 
 //Engineer
-const engineerTemplate = (employees) => 
-  `
-    <div class="col-sm-4" style="margin-top: 50px;">
-      <div class ="card" style="width: 20rem;">
-        <div class="card-header bg-primary">
-          <h1 class="card-title bg-primary">${employees.name}</h1>
-          <h2 class = "card-subtitle"><i class="fas fa-glasses"></i>${employees.getRole()}<h2>
-        </div>
-        <div class ="card-body">
-          <ul class="list-group text-left">
-           <li class="list-group-item">ID:${employees.id}</li>
-           <li class="list-group-item">Email:<a href="mailto:${employees.email}">${employees.email}</a></li>
-           <li class="list-group-item">GitHub:<a target="_blank" href="https://github.com/${employees.github}">Github</a></li>
-          </ul>
-        </div>
-        </div>
-      </div>
-`;
+const engineerTemplate = (employees) => {
+  return `
+  <div class="col-sm-4" style="margin-top: 50px;">
+<div class ="card" style="width: 20rem;">
+<div class="card-header bg-primary">
+<h1 class="card-title bg-primary text-white">${employees.name}</h1>
+<h2 class = "card-subtitle text-white"><i class="fas fa-glasses"></i>${employees.getRole()}<h2>
+</div>
+<div class ="card-body">
+  <ul class="list-group text-left">
+    <li class="list-group-item">ID: ${employees.id}</li>
+    <li class="list-group-item">Email: <a href="mailto:${employees.email}">${employees.email}</a></li>
+    <li class="list-group-item">GitHub: <a target="_blank" href="https://github.com/${employees.github}">${employees.github}</a></li>
+  </ul>
+  </div>
+  </div>
+</div>
+`};
 
 //Intern
 const internTemplate = (employees) => `
 <div class="col-sm-4" style="margin-top: 50px;">
-  <div class ="card" style="width: 20rem;">
-    <div class="card-header bg-primary">
-      <h1 class="card-title bg-primary">${employees.name}</h1>
-      <h2 class = "card-subtitle"><i class="fas fa-user-graduate"></i>${employees.getRole()}<h2>
-    </div>
-  <div class ="card-body">
-    <ul class="list-group text-left">
-      <li class="list-group-item">ID:${employees.id}</li>
-      <li class="list-group-item">Email:<a href="mailto:${employees.email}">${employees.email}</a></li>
-      <li class="list-group-item">School:${employees.school}</li>
-    </ul>
+<div class ="card" style="width: 20rem;">
+<div class="card-header bg-primary">
+<h1 class="card-title bg-primary text-white">${employees.name}</h1>
+<h2 class = "card-subtitle text-white"><i class="fas fa-user-graduate"></i>${employees.getRole()}<h2>
+</div>
+<div class ="card-body">
+<ul class="list-group text-left">
+<li class="list-group-item">ID: ${employees.id}</li>
+<li class="list-group-item">Email: <a href="mailto:${employees.email}">${employees.email}</a></li>
+<li class="list-group-item">School: ${employees.school}</li>
+</ul>
   </div>
 </div>
 </div>
@@ -70,29 +70,33 @@ const generatePage = (employees) => `
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
 </head>
 <body>
-<div class="text-center">
-<div class="p-4 mb-2 bg-danger text-white">My Team</div>
-  <div class="row container" style="margin: 2em auto 2em;">
-    <div class="row justify-content-md-center">
+<div class="container-fluid p-0 mb-0">
+        <div class="jumbotron jumbotron-fluid bg-danger text-light">
+            <div class="container text-center">
+                <h1 class="display-4">My Team</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row justify-content-center" id="cards">
       ${employees.map((person) => {
   console.log(`person.role: ${person.getRole()}`)
   if (person.getRole() === 'Manager') {
-      console.log('generating manager');
-      return managerTemplate(person)
-    }
-    else if (person.getRole() === 'Engineer') {
-      console.log('generating Engineer');
-      return engineerTemplate(person)
-    }
-    else {
-      console.log('person');
-        return internTemplate(person)
-      }
-      }).join("")
-    }
-    </div>
+    console.log('generating manager');
+    return managerTemplate(person)
+  }
+  else if (person.getRole() === 'Engineer') {
+    console.log('generating Engineer');
+    return engineerTemplate(person)
+  }
+  else {
+    console.log('person');
+    return internTemplate(person)
+  }
+}).join("")
+  }
   </div>
-</div>
+  </div>
+  </div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
